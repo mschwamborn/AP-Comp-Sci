bab0093347c1c9a7d74b5901b7556446abbc1a59
mschwamborn <32498883+mschwamborn@users.noreply.github.com>
2017-10-13 16:34:50 -0700

    Update Main.java

--- Edhesive/src/test/Main.java
+++ Edhesive/src/test/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		//test
+		//test test
 		String name;
 		int one, two, three;
 		Scanner sc = new Scanner (System.in);
