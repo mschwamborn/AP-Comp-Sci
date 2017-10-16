633d62604be8c2fa473288ff800f500f86e16b37
mic.sch001 <mic.sch001@HHS205D-G031.403.edu>
2017-10-16 13:08:53 -0700

    ComiT TestInfg

--- Edhesive/src/test/Class1.java
+++ Edhesive/src/test/Class1.java
@@ -32,7 +32,7 @@ public class Class1 {
 	    	four = ((one-three)/100);
 	    	System.out.print("Here are the digits:"+"\n"+four+"\n"+three+"\n"+two);*/
 	    	CrackCode();
-	    	
+	    	//STEST
 	    
 	    	
 	    	
