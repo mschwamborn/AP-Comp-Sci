a7b0b3a98b1282f87485e1cf414dc44fc13d9471
mic.sch001 <mic.sch001@HHS205D-G031.403.edu>
2017-10-20 14:02:20 -0700

    Edhesive Lesson 24

--- Edhesive/src/test/Main.java
+++ Edhesive/src/test/Main.java
@@ -5,23 +5,18 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		
-		int num = 23, count = 23;
-		        while(count<89) {
-		        	
-		            for(int o  = 0; o <10;o++){
-		            
-		            
-		                 System.out.print(count+" ");
-		                 
-		             count++;
-		             
-		            }
-		            System.out.print("\n");
-		        }
 		
-		/*for(int i =2; i <=50;i+=2) {
-			System.out.println(i);
-		}*/
+		        
+		        
+		        int countTen = 0;
+		       for(int count=23; count<=89;count++) {
+		        	System.out.print(count+" ");
+		        	countTen++;
+		        	if(countTen==10) {
+		        		System.out.println();
+		        		countTen=0;
+		        	}
+		        }
 		
 	    
 
