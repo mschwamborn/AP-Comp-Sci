c3a772d22bc8f3b22f777ceffef752c3b3722295
mic.sch001 <mic.sch001@HHS205D-G031.403.edu>
2017-10-31 11:59:51 -0700

    lunxch

--- Edhesive/src/test/Main.java
+++ Edhesive/src/test/Main.java
@@ -1,19 +1,20 @@
 package test;
 
 import java.util.*;
-
+//{"O ne", "T w o", " Th ree ",};
 public class Main {
+	 public static String [] list = {"every", " near  ing ", "      checking", "food ", "stand", "value      "};
 	public static void main(String[] args) 
 	{
-		String list [] = {"every", "nearing", "checking", "food", "stand", "value"};
-		
-		String word = null;
-		for (int i=0; i < list.length; i++)
-		{
-			word = new StringBuilder(list[i]).reverse().toString();
-			System.out.println(word);
-		
+		for( int i =0;i<list.length;i++) {
+			for(int u = 0; u<list[i].length();u++) {
+				if(list[i].charAt(u)==' ') {
+					
+			}
+			
+			}
+		}
+		System.out.print(list[0]+list[1]+list[2]+list[3]+list[4]+list[5]);
 
 	}
-}
 }
