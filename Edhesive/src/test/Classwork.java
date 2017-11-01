4cbc9b378a563efc08895647453878b36f1933cc
mic.sch001 <mic.sch001@HHS205D-G015.403.edu>
2017-11-01 14:02:01 -0700

    Finished Coding Activity 30

--- Edhesive/src/test/Classwork.java
+++ Edhesive/src/test/Classwork.java
@@ -560,7 +560,26 @@ public class Classwork {
 	    		
 	    	}
 	    }
-	    	
+	    	public static void RemoveSpace() {
+	    		String [] list = {"O ne", "T w o", " Th ree ",};
+	    		for( int i =0;i<list.length;i++) {
+	    			String word = "";
+	    			for(int u = 0; u<list[i].length();u++) {
+	    				if(list[i].charAt(u)!=' ') {
+	    				word += list[i].charAt(u);
+	    				}
+	    					
+	    			}
+	    				list[i] = word;
+	    				
+
+	    			
+	    			
+
+	    		}
+	    		System.out.print(list[0]+list[1]+list[2]);
+
+	    	}
 	    	
 
 }
--- Edhesive/src/test/Main.java
+++ Edhesive/src/test/Main.java
@@ -2,19 +2,28 @@ package test;
 
 import java.util.*;
 //{"O ne", "T w o", " Th ree ",};
+//test
 public class Main {
-	 public static String [] list = {"every", " near  ing ", "      checking", "food ", "stand", "value      "};
+	 public static String [] list = {"O ne", "T w o", " Th ree ",};
 	public static void main(String[] args) 
 	{
+		
 		for( int i =0;i<list.length;i++) {
+			String word = "";
 			for(int u = 0; u<list[i].length();u++) {
-				if(list[i].charAt(u)==' ') {
+				if(list[i].charAt(u)!=' ') {
+				word += list[i].charAt(u);
+				}
 					
 			}
+				list[i] = word;
+				
+
 			
-			}
+			
+
 		}
-		System.out.print(list[0]+list[1]+list[2]+list[3]+list[4]+list[5]);
+		System.out.print(list[0]+list[1]+list[2]);
 
 	}
 }
