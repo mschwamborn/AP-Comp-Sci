36db57c36720c51ea6f72dbffa0dcd6c7f03d7f5
mschwamborn <mikems@live.com>
2017-11-16 14:00:19 -0800

    Finished Coding Activity 34 and Classwork 1

--- Edhesive/src/test/Main.java
+++ Edhesive/src/test/Main.java
@@ -8,10 +8,9 @@ public class Main {
 	 public static String [] strg;
 	 //public static int [] ints = new int[3];
 	public static void main(String[] args) 
-	{int [] ints = {1,2,3};
+	{int [] ints = {55,2,15,6};
 		 
-insertValue(ints,1,2);
-
+	System.out.println(sumEven(ints));
 	
 	}
 	 
@@ -40,13 +39,69 @@ insertValue(ints,1,2);
 			
 		}
 	
-	public static void insertValue(int[] array, int data, int place) {
-		for( int i= array.length-1; i>place;i--) {
-			array[i] =array[i-1];
+	public static double average(int [] array) {
+		double avg = 0.0;
+		int sum =0;
+		for(int i =0;i<array.length;i++) {
+			
+			sum+=array[i];
+			avg = (double)sum/array.length;
 		}
-		 array[place]  =data;
+	
+		return avg;
 	}
-
+	
+	public static int findMax(int [] array) {
+		int largest =0;
+		for(int i =0;i<array.length;i++) {
+			if(array[i]>largest) {
+				largest = array[i];
+			}
+			
+			
+		}
+	
+		return largest;
+	}
+	public static int findMin(int [] array) {
+		int smallest =99999;
+		for(int i =0;i<array.length;i++) {
+			if(array[i]<smallest) {
+				smallest = array[i];
+			}
+			
+			
+		}
+		
+		return smallest;
+	}
+	public static int sumEven(int [] array) {
+		int sum =0;
+		for(int i =0;i<array.length;i++) {
+			if(array[i]%2==0) {
+				sum += array[i];
+			}
+			
+			
+		}
+		
+		return sum;
+	}
+	public static boolean allPositive(int [] array) {
+		boolean pos = true;
+		for(int i =0;i<array.length;i++) {
+			if(array[i]<=0) {
+				pos = false;
+				break;
+			}
+			
+			
+		}
+		
+		return pos;
+	}
+	
+	
 	
 
 	
--- Edhesive/src/test/arrayFun.java
+++ Edhesive/src/test/arrayFun.java
@@ -6,12 +6,13 @@ public class arrayFun {
 
 	public static void main(String[] args) {
 		
-		int[] array1 = {7,4,1,0,1,7,6,5,3,2,9,7};
+		int[] array1 = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
 		int[] array2 = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
 		int start1 =3; 
 		int stop1 =6; 
 		int start2 =2; 
 		int stop2 =9; 
+		int stop3 =16; 
 		System.out.println(Arrays.toString(array1));
 		System.out.println("sum of spots 3-6 = "+arrayFun.arraySum(array1,start1,stop1));
 		System.out.println("sum of spots 2-9 = "+arrayFun.arraySum(array1,start2,stop2));
@@ -20,18 +21,18 @@ public class arrayFun {
 		System.out.println(" # of 9s  = "+arrayFun.numOfNum(array1, 9));
 		System.out.println(" # of 7s  = "+arrayFun.numOfNum(array1, 7));
 		
-		System.out.println("new array with all 7s removed = "+Arrays.toString(arrayFun.deletedArray(array1,7)));
+		System.out.println("\nnew array with all 7s removed = "+Arrays.toString(arrayFun.deletedArray(array1,7)));
 		System.out.println(" # of 7s  = "+arrayFun.numOfNum(arrayFun.deletedArray(array1,7), 7));
 		
-		System.out.println(Arrays.toString(array2));
-		System.out.println("sum of spots 3-6 = "+arrayFun.arraySum(array2,start1,stop1));
+		System.out.println("\n\n"+Arrays.toString(array2));
+		System.out.println("sum of spots 3-16 = "+arrayFun.arraySum(array2,start1,stop3));
 		System.out.println("sum of spots 2-9 = "+arrayFun.arraySum(array2,start2,stop2));
 		
-		System.out.println("\n # of 4s  = "+arrayFun.numOfNum(array2, 4));
-		System.out.println(" # of 9s  = "+arrayFun.numOfNum(array2, 9));
+		System.out.println("\n # of 0s  = "+arrayFun.numOfNum(array2, 0));
+		System.out.println(" # of 3s  = "+arrayFun.numOfNum(array2, 3));
 		System.out.println(" # of 7s  = "+arrayFun.numOfNum(array2, 7));
 		
-		System.out.println("new array with all 7s removed = "+Arrays.toString(arrayFun.deletedArray(array2,7)));
+		System.out.println("\nnew array with all 7s removed = "+Arrays.toString(arrayFun.deletedArray(array2,7)));
 		System.out.println(" # of 7s  = "+arrayFun.numOfNum(arrayFun.deletedArray(array2,7), 7));
 		
 		
@@ -64,13 +65,20 @@ public class arrayFun {
 		
 	}
 	public static int [] deletedArray(int [] array, int badNum) {
-		int numofbadNum = arrayFun.numOfNum(array, badNum);
-		int [] newArray = new int[array.length - numofbadNum];
-		int arrayIndex = 0;
-		for(int i = 0; i<newArray.length;i++) {
-			if(array[i]!=badNum) {
-				newArray[arrayIndex] = array[i];
-				arrayIndex++;
+		int numOfNum = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == badNum) {
+				numOfNum++;
+			}
+		}
+		
+		
+		int[] newArray = new int [(array.length-numOfNum)];
+		int newArrayIndex = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] != badNum) {
+				newArray[newArrayIndex]=array[i];
+				newArrayIndex++;
 			}
 		}
 		
