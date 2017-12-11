f3c688c15232c56006f00f625090394829a17dda
mschwamborn <mikems@live.com>
2017-12-10 21:26:04 -0800

    Worked on L 4 coding activity not working in edhesive

--- Edhesive/src/test/Vehicle.java
+++ Edhesive/src/test/Vehicle.java
@@ -3,16 +3,50 @@ package test;
 public class Vehicle {
 	int location;
 	
-	public Vehicle() {
+	
+	
+	public  Vehicle() {
 		location = 0;
+		toString();
 	}
 
-	public  Vehicle(int loc) {
-		if(loc> -20||loc< 20)
+	public   Vehicle(int loc) {
+		if(loc>= -20&&loc<= 20)
 			location = loc;
+		
 		else
 			location = 0;
 		
+	}
+	
+	public void forward() {
+		if(location < 20) {
+			location++;
+		}
+		else
+			location =20;
+	}
+	public void backward() {
+		if(location > -20) {
+			location--;
+		}
+		else
+			location =-20;
+	}
+	
+	public int getLocation() {
+		return location;
+	}
+	
+	public String toString() {
+		String loc = "";
+		
+		for(int i =0; i<location; i++) {
+			loc = loc + " ";
+		}
+		loc = loc + "@";
+		
+		return loc; 
 	}
 	
 	
